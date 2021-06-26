b337d79619a8c007fff2481289d455ec54f522c6
BrandonHaslam <BrandonHaslam@users.noreply.github.com>
2021-06-26 21:21:09 +0100

    Tailwind added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Hangman />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Hangman from "./components/Hangman/Hangman.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Hangman,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Hangman/Hangman.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1 class="uppercase">Hangman</h1>
+</template>
+<script>
+export default {
+  name: "Hangman",
+};
+</script>
